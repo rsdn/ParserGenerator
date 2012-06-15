1c18fc315ba10bcbd63099d4f15b81ef28c4a473
Visor <xxVisorxx@gmail.com>
2012-06-15 12:34:29 +0300

    themes assembly added for xaml files

---
+++ N2VsExtentions/Rsdn.N2.VisualStudio.Themes/Generic.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rsdn.N2.VisualStudio.Themes
+{
+    public partial class Generic
+    {
+        public Generic()
+        {
+            InitializeComponent();
+        }
+    }
+}
